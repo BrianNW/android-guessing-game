d9bfb609e29742c52a04610fd53c77e3ff5430fa
Brian W <Briantechdom@gmail.com>
2018-05-27 13:23:28 -0700

    Fixed alignment issues. Is runnable now

--- app/src/main/java/ca/briannw/guessinggame/MainActivity.java
+++ app/src/main/java/ca/briannw/guessinggame/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -44,7 +45,8 @@ public class MainActivity extends AppCompatActivity {
             message = "Couldn't understand that. Try again.";
             lblOutput.setText(message);
         }finally { //highlight the txtGuess text field
-
+            txtGuess.requestFocus();
+            txtGuess.selectAll();
         }
     }
 
@@ -68,6 +70,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 checkGuess();
+            }
+        });
+//        set up the event listener for the input field
+        txtGuess.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
+                checkGuess();
+                return true;
             }
         });
 
